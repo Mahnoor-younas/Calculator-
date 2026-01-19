98d19fc81fb68c174d7fc5bca2fad11ba030bc9e
Mahnoor-younas <aliyamehmood478@gmail.com>
2026-01-19 18:17:01 +0500

    Add files via upload

---
+++ css/apps.js
@@ -0,0 +1,13 @@
+ function append(value) {
+      document.getElementById("result").value += value;
+    }
+
+    function calculate() {
+      document.getElementById("result").value =
+        eval(document.getElementById("result").value);
+    }
+
+    function clearResult() {
+      document.getElementById("result").value = "";
+    }
+  
